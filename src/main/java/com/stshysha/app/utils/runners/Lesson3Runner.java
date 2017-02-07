36a700e84dec460f94fca9784e10c83b53cec843
stshysha <stshysha@gmail.com>
2017-02-07 15:54:19 +0200

    Change homework 3

--- src/main/java/com/stshysha/app/utils/runners/Lesson3Runner.java
+++ src/main/java/com/stshysha/app/utils/runners/Lesson3Runner.java
@@ -6,15 +6,11 @@ import com.stshysha.app.utils.convertors.PrimitiveConvertor;
  * Created by new on 05.02.17.
  */
 public class Lesson3Runner {
-    public static void main (String args[]){
+    public static void main (String args[]) {
 
-        PrimitiveConvertor casting1 = new PrimitiveConvertor();
-        casting1.floatToChar();
-
-        PrimitiveConvertor casting2 = new PrimitiveConvertor();
-        casting2.intToChar();
-
-        PrimitiveConvertor casting3 = new PrimitiveConvertor();
-        casting3.charToInt();
+        PrimitiveConvertor convertor1 = new PrimitiveConvertor();
+        convertor1.floatToChar(50.0f);
+        convertor1.intToChar(70);
+        convertor1.charToInt('a');
     }
 }
